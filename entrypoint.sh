e1ebe5aac153c8b3c5158bd8ecae7933751e8cc3
Kostiantyn Pinchuk <apollon@users.noreply.github.com>
2020-05-21 16:39:00 +0300

    trigger SSL certificates update when any config was changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 crond
 sh -c 'sleep 1 && ./update_ssl.sh' &
+sh -c 'while inotifywait -re create,delete,move,modify, /etc/nginx/conf.d/sites/; do cd /home/nginx; ./update_ssl.sh; done' &
 
 nginx -g 'daemon off;'
